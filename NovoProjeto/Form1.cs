753ae363ed801366d8404b459d85fe28740925aa
Gabri <Gabri@JOVEMGABE>
2021-07-26 20:49:14 -0300

    Adicionar arquivos de projeto.

---
+++ NovoProjeto/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NovoProjeto
+{
+    public partial class Form1 : Form
+    {
+        decimal saldoAtual = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            decimal addSaldo;
+
+            addSaldo = Convert.ToDecimal(textBox1.Text);            
+            saldoAtual = saldoAtual + addSaldo;
+
+            label3.Text = "R$ " + Convert.ToString(saldoAtual) + ".00";
+        }
+    }
+}
